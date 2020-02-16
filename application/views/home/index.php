2c09dd17c30fc9adee830a42c5bae7f39974f73d
unknown <yogiermanto59@gmail.com>
2020-02-16 14:21:09 +0700

    first commit

---
+++ application/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+      <p class="lead">Halo, nama saya <?= ! $nama == '' ? $nama : 'Yogi' ?></p>
+      <hr class="my-4">
+      <p>Website ini dibuat menggunakan Code Igniter</p>
+      <a class="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=dMRCZGGAx74&t=21s" role="button">Learn Tutorial</a>
+    </div>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<title><?=$judul?></title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container">
+			<a class="navbar-brand" href="<?=base_url()?>">CI-App</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a class="nav-item nav-link" href="<?=base_url()?>">Home <span class="sr-only">(current)</span></a>
+					<a class="nav-item nav-link" href="<?=base_url()?>mahasiswa">Mahasiswa</a>
+					<a class="nav-item nav-link" href="<?=base_url()?>about">About</a>
+				</div>
+			</div>
+		</div>
+	</nav>
